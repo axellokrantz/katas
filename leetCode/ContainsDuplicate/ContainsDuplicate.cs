7d023bb57c6e5062a4affd33c4b8a987ef5191be
axellokrantz <axel.lokrantz@gmail.com>
2024-07-10 19:57:06 +0200

    fix: folderstructure in place.

---
+++ leetCode/ContainsDuplicate/ContainsDuplicate.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public bool ContainsDuplicate(int[] nums) {
+
+        HashSet<int> set = new HashSet<int>();
+        foreach (var num in nums)
+        {
+            if(!set.Add(num))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = {1,2,3,4,4};
+        Solution s = new Solution();
+        Console.WriteLine(s.ContainsDuplicate(nums));
+    }
+}
---
+++ leetCode/TwoSum/TwoSum.cs
@@ -0,0 +1,34 @@
+﻿
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target) {
+          
+        int[] result = new int[2];
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        int compliment;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            compliment = target - nums[i];
+            if(dict.ContainsKey(target - compliment))
+            {
+                result[0] = dict[target - compliment];
+                result[1] = i;
+                return result;
+            }
+            if(!dict.ContainsKey(compliment))
+            {
+                dict.Add(compliment, i);
+            }
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        Solution s = new Solution();
+        Console.WriteLine(s.TwoSum(nums, target));
+    }
+}
