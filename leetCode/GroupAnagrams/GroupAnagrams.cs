44b853a76471c870083a6a17f215e112ec45214d
axellokrantz <axel.lokrantz@gmail.com>
2024-07-10 21:32:43 +0200

    create: solution for IsAnagram and GroupAnagrams

---
+++ leetCode/GroupAnagrams/GroupAnagrams.cs
@@ -0,0 +1,39 @@
+﻿
+public class Solution
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+        IList<IList<string>> list = new List<IList<string>>();
+
+        foreach (var str in strs)
+        {
+            char[] arr = str.ToCharArray();
+            Array.Sort(arr);
+            string sortedString = new string(arr);
+
+            if(!dict.ContainsKey(sortedString))
+            {
+                dict.Add(sortedString, new List<string>{str});
+            }
+            else
+            {
+                dict[sortedString].Add(str);
+            }
+        }
+
+        foreach (var group in dict.Values)
+        {
+            list.Add(group);
+        }
+        
+        return list;
+    }
+
+    static void Main(string[] args)
+    {
+        string[] strs = {"eat","tea","tan","ate","nat","bat"};
+        Solution sol = new Solution();
+        sol.GroupAnagrams(strs);
+    }
+}
---
+++ leetCode/IsAnagram/IsAnagram.cs
@@ -0,0 +1,59 @@
+﻿
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length)
+        {
+            return false;
+        }
+
+        Dictionary<char, int> sDict = new Dictionary<char, int>();
+        Dictionary<char, int> tDict = new Dictionary<char, int>();
+
+        char[] sCharArray = s.ToCharArray();
+        char[] tCharArray = t.ToCharArray();
+
+        for (int i = 0; i < sCharArray.Length; i++)
+        {
+            if (!sDict.ContainsKey(sCharArray[i]))
+            {
+                sDict.Add(sCharArray[i], 1);
+            }
+            else
+            {
+                sDict[sCharArray[i]]++;
+            }
+        }
+
+        for (int i = 0; i < tCharArray.Length; i++)
+        {
+            if (!tDict.ContainsKey(tCharArray[i]))
+            {
+                tDict.Add(tCharArray[i], 1);
+            }
+            else
+            {
+                tDict[tCharArray[i]]++;
+            }
+        }
+
+        foreach (var valuePair in sDict)
+        {
+            if(!tDict.ContainsKey(valuePair.Key) || tDict[valuePair.Key] != valuePair.Value)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        string s = "anagram";
+        string t = "nagaram";
+        Solution sol = new Solution();
+        Console.WriteLine(sol.IsAnagram(s, t));
+    }
+}
