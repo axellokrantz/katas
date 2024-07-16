62aa413e6abb24ed6ffa38e74b27e433301da70e
axellokrantz <axel.lokrantz@gmail.com>
2024-07-16 23:36:10 +0200

    MaxProfit complete

---
+++ neetCode/MaxProfit/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution {
+    
+    public int MaxProfit(int[] prices) {
+        
+        int maxProfit = 0;
+        int start = 0;
+        int end = 1;
+       
+        while(end < prices.Length)
+        {
+            if(prices[start] > prices[end])
+            {
+                start = end;
+            }
+            else
+            {
+                maxProfit = Math.Max(maxProfit, prices[end] - prices[start]);
+            }
+            end++;
+        }
+        return maxProfit;
+    }
+
+    static void Main()
+    {
+        int[] prices = [10,1,5,6,7,1];
+        Solution s = new();
+        Console.WriteLine(s.MaxProfit(prices));
+    }
+}
