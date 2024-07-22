405bc7d00d000cdb05093ad705f92256208855dd
axelml <axel.lokrantz@gmail.com>
2024-07-22 09:33:32 +0200

    TopKFrequent almost done.

---
+++ neetCode/TopKFrequent/TopKFrequent.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        Dictionary<int, int> dict = new();
+        int[] result = new int[k];
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(!dict.ContainsKey(nums[i]))
+            {
+                dict.Add(nums[i], 1);
+            }
+            else
+            {
+                dict[nums[i]]++;
+            }
+        }
+
+        var sortedByFreqeuency = dict.OrderBy(x => x.Value).ToList();
+
+        for (int i = 0; i < k; i++)
+        {
+            
+        }
+
+        return [];
+    }
+
+    static void Main()
+    {
+        Solution s = new();
+        s.TopKFrequent([1, 2, 2, 3, 3, 3], 2);
+    }
+}
