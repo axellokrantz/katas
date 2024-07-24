907ef099f0582a5ce7ad1186f56336078bbc8390
axellokrantz <axel.lokrantz@gmail.com>
2024-07-24 23:20:06 +0200

    new

---
+++ neetCode/TwoSumII/TwoSumII.cs
@@ -0,0 +1,29 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+       int start = 0;
+       int end = numbers.Length - 1;
+
+       while(start < end)
+       {
+            if(numbers[start] + numbers[end] == target)
+            {
+                return [start + 1, end + 1];
+            }
+            else if(numbers[start] + numbers[end] > target)
+            {
+                end--;
+            }
+            else
+            {
+                start++;
+            }
+       }
+       throw new Exception("No indices found for the specified target.");
+    }
+
+    static void Main()
+    {
+        Solution s = new();
+        s.TwoSum([2,7,11,15], 5);
+    }
+}
