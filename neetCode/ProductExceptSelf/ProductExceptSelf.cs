3273fd217eb18b01820ab9eda6f68c4ec7761b6c
axelml <axel.lokrantz@gmail.com>
2024-07-25 21:00:27 +0200

    new

---
+++ neetCode/ProductExceptSelf/ProductExceptSelf.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    // 1,2,3,4
+    // ^ 
+    // i 
+    //   j
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int sum = 1;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if(j == i)
+                {
+                    continue;
+                }
+                sum *= nums[j];
+            }
+
+            result[i] = sum;
+        }
+        foreach (int item in result)
+        {
+            Console.WriteLine(item);
+        }
+        return result;
+    }
+
+    static void Main()
+    {
+        Solution s = new();
+        s.ProductExceptSelf([1, 2, 4, 6]);
+    }
+}
---
+++ neetCode/ThreeSum/ThreeSum.cs
@@ -0,0 +1,27 @@
+﻿public class Solution {
+    public List<List<int>> ThreeSum(int[] nums)
+    {
+        List<List<int>> result = new();
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            HashSet<int> set = new();
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                int target = -(nums[i] + nums[j]);
+                if(set.Contains(target)){
+                    result.Add(new List<int> {nums[i], nums[j], target});
+                }
+                else{
+                    set.Add(nums[j]);
+                }
+            }
+        }
+        return result;
+    }
+
+    static void Main(){
+        Solution s = new();
+        s.ThreeSum([-1,0,1,2,-1,-4]);
+    }
+}
